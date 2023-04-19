79863e0fa33c3bd27f62c1934118fea5e6a7ced0
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-04-19 12:12:22 -0400

    Created search view and the view group.

---
+++ FinalModTaskDictionary/Views/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  FinalModTaskDictionary
+//
+//  Created by Lucas García on 19/4/23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
